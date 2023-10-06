7cf5dcf28724e5e0e93ff087551b167e329f25f4
suhellynaraujo <github.interface@gmail.com>
2023-10-05 23:01:40 -0300

    separando

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,3 @@
-
-const offse = 0;
-const limit = 10;
-const url = "https://pokeapi.co/api/v2/pokemon?offset=0&limit=10"
-
 // processamento assicrono, promessa de uma resposta
 //o fetch processa, aguardando a resposta do servidor, depois executa a função de callback (function(response))
 /*fetch(url)
@@ -18,7 +13,7 @@ const url = "https://pokeapi.co/api/v2/pokemon?offset=0&limit=10"
     .then(function(jsonBody){
         console.log(jsonBody)
     })
-    
+
     .catch(function (error) {
         console.log(error)
     })
@@ -27,8 +22,45 @@ const url = "https://pokeapi.co/api/v2/pokemon?offset=0&limit=10"
         console.log("Requisição concluida!")
     })*/
 
-    //encadeamento
-    fetch(url)
+//encadeamento
+function convertPokemonToLi(pokemon) {
+    return `
+        <li class="pokemon">
+        <span class="number">#001</span>
+        <span class="name">${pokemon.name}</span>
+        <div class="detail">
+            <ol class="types">
+                <li class="type">grass</li>
+                <li class="type">poison</li>
+            </ol>
+            <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg"
+                alt="${pokemon.name}">
+        </div>
+    </li>`
+}
+
+const pokemonList = document.getElementById('pokemonList')
+/*
+fetch(url)
     .then((response) => response.json())
-    .then((jsonBody) => console.log(jsonBody))
-    .then((error) => console.error(error))
+    .then((jsonBody) => jsonBody.results)
+    .then((pokemons) => {
+        /*debugger
+        console.log(pokemonList)
+
+        for (let index = 0; index < pokemons.length; index++) {
+            const pokemon = pokemons[index];
+            pokemonList.innerHTML += (convertPokemonToLi(pokemon))            
+        }
+    })
+    .catch((error) => console.error(error))*/
+
+pokeAPI.getPokemons().then((pokemons) => { 
+
+    for (let index = 0; index < pokemons.length; index++) {
+        const pokemon = pokemons[index];
+        pokemonList.innerHTML += (convertPokemonToLi(pokemon))            
+    }
+})
+
+
